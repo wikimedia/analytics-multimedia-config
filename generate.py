75008e1acbc3185f98f0688280735e01c092afe2
Gergő Tisza <tgr.huwiki@gmail.com>
2014-07-20 20:39:00 -0700

    Remove generated files from source control
    Change-Id: Iafc2c947d8723de465b54f85d49da751aac79da5

---
+++ generate.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+wikis = [
+    'global',
+    'cawiki',
+    'commonswiki',
+    'czwiki',
+    'dewiki',
+    'enwiki',
+    'enwikivoyage',
+    'eswiki',
+    'etwiki',
+    'fiwiki',
+    'frwiki',
+    'hewiki',
+    'huwiki',
+    'itwiki',
+    'jawiki',
+    'kowiki',
+    'mediawikiwiki',
+    'nlwiki',
+    'plwiki',
+    'ptwiki',
+    'rowiki',
+    'ruwiki',
+    'skwiki',
+    'svwiki',
+    'tewiki',
+    'thwiki',
+    'viwiki',
+]
+
+performance_metrics = {
+    'userinfo': 'API call fetching information about users. Mainly used for correct gender-based localization of text',
+    'imageinfo': 'API call fetching general information about the image',
+    'thumbnailinfo': 'API call fetching information about a thumbnail of a specific size for the image',
+    'filerepoinfo': 'API call fetching information about a file repository',
+    'imageusage': 'API call fetching file usage information on the local wiki',
+    'globalusage': 'API call fetching file usage information on all wikis but the local one',
+    'image': 'images shown in Media Viewer',
+    'imagehit': 'images shown in Media Viewer, cache hit (image didn\'t have to be generated on the fly)',
+    'imagemiss': 'images shown in Media Viewer, cache miss (image had to be generated on the fly)',
+}
+
+geoperformance_metrics = {
+    'api': 'Average duration of API calls per country, in milliseconds. The lower, the better.',
+    'image': 'Average duration of image loads per country, in milliseconds. The lower, the better.'
+}
+
+templates = {
+    './dashboards/mmv':  None,
+    './datasources/mmv_performance': performance_metrics,
+    './graphs/mmv_performance': performance_metrics,
+    './datasources/mmv_geoperformance': geoperformance_metrics,
+    './graphs/mmv_geoperformance': geoperformance_metrics,
+    './datasources/mmv_actions': None,
+    './graphs/mmv_actions': None,
+    './graphs/mmv_opt_inout': None,
+}
+
+def generate(wiki, templates):
+    if not os.path.isfile('./datasources/mmv_performance_template.json'):
+        print('You must run this script from the multimedia-limn folder')
+        sys.exit(1);
+
+    for template, metrics in templates.items():
+        json = open(template + '_template.json').read(10000)
+        json = json.replace('%wiki%', wiki)
+
+        if metrics:
+            for metric, metric_description in metrics.items():
+                metric_json = json.replace('%metric%', metric).replace('%metricdescription%', metric_description)
+                with open(template + '_' + metric + '_' + wiki + '.json', 'w') as f:
+                    f.write(metric_json)
+        else:
+            with open(template + '_' + wiki + '.json', 'w') as f:
+                f.write(json)
+
+for wiki in wikis:
+    generate(wiki, templates)
